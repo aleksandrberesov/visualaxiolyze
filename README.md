1d59498f7219210d78d0ab5756346194c9c142c1
a.beresov <aleksandr.beresov@gmail.com>
2026-04-20 10:29:44 -0300

    remove useless files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # axiolyze
 adapter between glm and vdag
+
+## install 
+
+git submodule update --init --recursive
+git submodule sync --recursive
+
+python -m venv .dev
+.dev\Scripts\Activate
--- check_repos.py
+++
@@ -1,8 +0,0 @@
-import setup_imports
-
-from deps.repo_vdag.src.utils.common import safe_some_function
-from deps.repo_glm.glm_transformers import GLMTransformation
-
-res = safe_some_function(1)
-print("Result: ", res)
-
